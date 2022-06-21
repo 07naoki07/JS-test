faa34037604754334cb0a7f501a4b2da8e7024a7
07naoki07 <naokiti.0205.22@gmail.com>
2022-06-21 22:13:36 +0900

    updadte

---
+++ test.js
@@ -0,0 +1,25 @@
+
+/*
+この間は全てコメントアウトされる
+*/
+
+// コマンド＋/でコメントアウトされる
+
+// 変数　変わる数　variable(var)
+// 昔使用されていた
+// var test = 123;
+// var test = 'test';
+
+// let test =123;
+// test += 123;
+
+const test = 123; //constant 定数
+const test_2 = 'テスト';//文字
+
+//動的型付け言語　変数中身を自動的に判定
+//静的型付け言語　Java Visual Basic
+
+//変数の頭文字は　英語または数字または＄＄
+
+
+console.log(typeof test);
