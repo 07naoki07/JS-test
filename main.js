338d5e6eaf8ec08fb6059a1312fc1066c92e8f91
07naoki07 <naokiti.0205.22@gmail.com>
2022-06-29 23:59:59 +0900

    update

---
+++ main.js
@@ -0,0 +1,18 @@
+//if (条件){
+//　条件が真なら実行
+//}　else{
+//  　条件が偽なら実行
+//}
+
+const height =91;
+
+console.log(typeof height);
+
+if (height === 90){
+  console.log('身長は'+ height + 'cmです');
+} else {
+  console.log('身長は90cmではありません');
+}
+
+// == 一致
+// === 型も一致 　こちらが主流で使う
